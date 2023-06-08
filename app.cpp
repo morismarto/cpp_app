72ca8b3fff6b7ddd1b589cc5aeb7b681abc6dd64
morismarto <moir.meir@bk.ru>
2023-06-08 18:54:22 +0400

    second commit

--- app.cpp
+++ app.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
+
 int main() {
-    cout << "Hello world\n";
+    cout << "Hello world" << endl;
     return 0;
 }
